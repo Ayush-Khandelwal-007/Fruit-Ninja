6ee2f05450afad3a9d4f04ea87d8a68b0e1c06ae
Ayush Khandelwal <ayush@Ayushs-MacBook-Air.local>
2020-07-31 20:36:50 +0530

    added images for fruits bomb and the play buton starts working

--- jquery.js
+++ jquery.js
@@ -1,6 +1,8 @@
 var isPlaying=false;
 var score =0;
 var lives;
+
+var fruits = ['apple', 'banana', 'papaya', 'grapes', 'mango', 'orange', 'peach', 'pear', 'pineapple','tomato','watermelon'];
 $(function(){
     
     $("#start").click(function(){
@@ -13,11 +15,19 @@ $(function(){
             $("#value").html(score);
             $("#menubar").hide();
             $("#container").show();
-
             lives=3;
-            for(i=0; i<3 ; i++){
-                $("#life").append("H ");        
-            }
+            addHeart();
+            startFruits();
         }
     });
 });
+
+function addHeart(){
+    for(i=0;i<lives;i++){
+        $("#life").append(`<img src="images/heart.png" class="heart">`);
+    }
+}
+
+function startFruits(){
+
+}
