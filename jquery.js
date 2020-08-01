7e2530a9561fe0123f55f0c26a402afcb16766a9
Ayush Khandelwal <ayush@Ayushs-MacBook-Air.local>
2020-08-01 17:22:16 +0530

    added game ending box and fruit started to fall and cut

--- jquery.js
+++ jquery.js
@@ -1,8 +1,10 @@
 var isPlaying=false;
 var score =0;
 var lives;
+var dropSpeed=1;
+var action;
 
-var fruits = ['apple', 'banana', 'papaya', 'grapes', 'mango', 'orange', 'peach', 'pear', 'pineapple','tomato','watermelon'];
+var fruits = ['apple', 'banana', 'grapes', 'mango', 'orange', 'peach', 'pear', 'pineapple','tomato','watermelon'];
 $(function(){
     
     $("#start").click(function(){
@@ -14,7 +16,7 @@ $(function(){
             score=0;
             $("#value").html(score);
             $("#menubar").hide();
-            $("#container").show();
+            $("#container").css({'display':'flex'});
             lives=3;
             addHeart();
             startFruits();
@@ -22,12 +24,63 @@ $(function(){
     });
 });
 
+$("#fruit").mouseover(function(){
+    score++;
+    $("#value").html(score);
+    //$("#slicesound")[0].play();
+    
+    clearInterval(action);
+    
+
+    $("#fruit").hide("explode", 500); 
+    
+    setTimeout(startFruits, 80);
+});
+
 function addHeart(){
+    $("#life").empty();
     for(i=0;i<lives;i++){
         $("#life").append(`<img src="images/heart.png" class="heart">`);
     }
 }
 
 function startFruits(){
+    $("#fruit").show;
+    chooseFruit();
+    $("#fruit").css({'left' : Math.round(($("#container").width()-150)*Math.random())+200, 'top' : -50}); 
 
+    dropSpeed+=1;
+    action = setInterval(function(){
+        
+        $("#fruit").css('top', $("#fruit").position().top + dropSpeed);
+        if($("#fruit").position().top > $("#container").height()){
+            
+            if(lives > 1 ){
+                
+                $("#fruit").show();
+                chooseFruit();
+                $("#fruit").css({'left' : Math.round(($("#container").width()-150)*Math.random())+200, 'top' : -50});
+                dropSpeed+=1;
+                
+                lives-=1;
+                
+                addHeart();
+                
+            }else{ 
+                playing = false;
+                $("#fsc").text(score);
+                $("#endgame").show();
+                stopAction();
+            }
+        }
+    },10)
+}
+
+function chooseFruit(){
+    $("#fruit").attr('src' , 'images/' + fruits[Math.round(9*Math.random())] +'.png');
+}
+
+function stopAction(){
+    clearInterval(action);
+    $("#fruit").hide();
 }
